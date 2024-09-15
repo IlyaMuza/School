c35864d3ee97cdb26120bfeeb71d1032faab33ad
IlyaMuza <vasilijcapaev665@gmail.com>
2024-09-15 13:43:58 +0300

    Add files via upload

--- mod_5_1.py
+++ mod_5_1.py
@@ -11,6 +11,12 @@ class House:
             for i in range(1, new_floor+1):
                 print(f'Дом \'{self.name}\' этаж {i}')
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
@@ -22,3 +28,7 @@ h4 = House('Калининский', 10)
 h3.go_to(7)
 h4.go_to(12)
 h3.go_to(-2)
+
+print(len(h1),len(h2), len(h3))
+print(str(h4))
+print(str(h3))
