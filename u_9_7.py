f62924cda0406c63f473f8fd93fd2bae8d5492ca
IlyaMuza <vasilijcapaev665@gmail.com>
2024-10-25 20:49:47 +0300

    Add files via upload

---
+++ u_9_7.py
@@ -0,0 +1,16 @@
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)
+        for i in range(2, int(result**0.5)+1):
+            if result % i == 0:
+                return False
+        return True
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+res = sum_three(2,1,8)
+print(res)
