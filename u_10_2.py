8ca0c4a6f8340c59b86dff430aa8dbbb7a4292f3
IlyaMuza <vasilijcapaev665@gmail.com>
2024-10-28 16:24:58 +0300

    Add files via upload

---
+++ u_10_2.py
@@ -0,0 +1,28 @@
+import threading
+import  time
+
+class Knight(threading.Thread):
+
+    def __init__(self, name, power):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        healph, day = 100, 0
+        while healph > 0:
+            time.sleep(0.5)
+            healph -= self.power
+            day += 1
+            print(f'{self.name} сражается {day} дней, осталось {healph} вражеских воинов.')
+        print(f'{self.name} одержал победу сапустя {day} дней(дня)!')
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight('Sir Galahad', 20)
+first_knight.start()
+second_knight.start()
+first_knight.join()
+second_knight.join()
+print('Все битвы закончились!')
+
